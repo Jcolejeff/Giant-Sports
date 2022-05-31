bfd16d409e8eabeb39293dc18fee148d36cad49e
jcolejeff <ikwuhj@gmail.com>
2022-05-31 21:08:27 +0100

    set up project structure

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+function App() {
+  return <h1>React Router 6 Tutorial</h1>;
+}
+
+export default App;
+
+
