987e54328195cbb75d7edaa6c59b95a0b5efe0d2
jcolejeff <ikwuhj@gmail.com>
2022-06-07 06:29:46 +0100

    added error page color

--- src/pages/ComingSoon.js
+++ src/pages/ComingSoon.js
@@ -2,12 +2,15 @@ import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 import { useGlobalContext } from "../context";
-
+import { IoConstructSharp } from "react-icons/io5";
 const ComingSoon = () => {
 	const { closeSubmenu } = useGlobalContext();
 
 	return (
 		<Wrapper className="page-100" onMouseOver={closeSubmenu}>
+			<h1>
+				<IoConstructSharp></IoConstructSharp>{" "}
+			</h1>
 			<h3>Sorry,the page is still under constuction </h3>
 
 			<Link to="/" className="btn">
@@ -17,7 +20,7 @@ const ComingSoon = () => {
 	);
 };
 
-const Wrapper = styled.main`
+const Wrapper = styled.section`
 	background: var(--clr-primary-10);
 	display: flex;
 	justify-content: center;
@@ -26,11 +29,12 @@ const Wrapper = styled.main`
 	flex-direction: column;
 
 	h1 {
-		font-size: 10rem;
+		font-size: 5rem;
 	}
 	h3 {
 		text-transform: none;
 		margin-bottom: 2rem;
+		width: 60%;
 	}
 	.btn {
 		padding: 0.75rem 1.5rem;
--- src/pages/Error.js
+++ src/pages/Error.js
@@ -7,29 +7,32 @@ const ErrorPage = () => {
 
 	return (
 		<Wrapper className="page-100" onMouseOver={closeSubmenu}>
-			<section>
-				<h1>404</h1>
-				<h3>Sorry,the page you tried cannot be found</h3>
-				<Link to="/" className="btn">
-					back home
-				</Link>
-			</section>
+			<h1>404</h1>
+			<h3>Sorry,the page you tried cannot be found</h3>
+			<Link to="/" className="btn">
+				back home
+			</Link>
 		</Wrapper>
 	);
 };
 
-const Wrapper = styled.main`
+const Wrapper = styled.section`
 	background: var(--clr-primary-10);
 	display: flex;
+	flex-direction: column;
 	justify-content: center;
 	align-items: center;
 	text-align: center;
 	h1 {
 		font-size: 10rem;
+		color: red;
 	}
 	h3 {
 		text-transform: none;
 		margin-bottom: 2rem;
+		width: 60%;
+		color: red;
+		text-align: center;
 	}
 	.btn {
 		padding: 0.75rem 1.5rem;
